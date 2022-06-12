3c664e023c3b025123bef2424576c73d4a228933
Cravsi <shaun.bonner1@gmail.com>
2022-06-12 22:35:51 +0000

    [add] JS screen selection functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Function to hide all other screens and show landing screen
+ */
+function landingScreen(){
+
+}
+
+/**
+ * Function to hide all other screens and show learn screen
+ */
+function learnScreen() {
+
+}
+
+/**
+ * Function to hide all other screens and show practice screen
+ */
+function practiceScreen(){
+
+}
+
+/**
+ * Function to hide all other screens and show review screen
+ */
+function reviewScreen (){
+
+}

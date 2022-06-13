b9524e3b6eb22b09d96640d2ee267ad02ca1ac8e
Cravsi <shaun.bonner1@gmail.com>
2022-06-13 14:02:30 +0000

    [add] hide other screen wshen page first loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
 /**
- * Function to hide all other screens and show landing screen
+ * Load JS content after page is loaded
  */
-function landingScreen(){
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const landingScreen = document.getElementById('landing-screen');
+    const learnScreen = document.getElementById('learn-screen');
+    const quizScreen = document.getElementById('quiz-screen');
+    const reviewScreen = document.getElementById('review-screen');
 
-}
+    goToLanding();
+    /**
+     * Function to hide all other screens and show landing screen
+     */
+    function goToLanding() {
+        landingScreen.classList.remove('hide');
+        learnScreen.classList.add('hide');
+        quizScreen.classList.add('hide');
+        reviewScreen.classList.add('hide');
+    }
 
-/**
- * Function to hide all other screens and show learn screen
- */
-function learnScreen() {
+    /**
+     * Function to hide all other screens and show learn screen
+     */
+    function learnScreen() {
 
-}
+    }
 
-/**
- * Function to hide all other screens and show practice screen
- */
-function practiceScreen(){
+    /**
+     * Function to hide all other screens and show practice screen
+     */
+    function practiceScreen() {
 
-}
+    }
 
-/**
- * Function to hide all other screens and show review screen
- */
-function reviewScreen (){
+    /**
+     * Function to hide all other screens and show review screen
+     */
+    function reviewScreen() {
 
-}
+    }
+})

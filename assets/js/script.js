69a73f6653be1a1cdeb4bc2f1061a996a8753d41
Cravsi <shaun.bonner1@gmail.com>
2022-06-14 18:23:02 +0000

    [add] page selection functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,41 +2,49 @@
  * Load JS content after page is loaded
  */
 document.addEventListener('DOMContentLoaded', function () {
-    
+
+
     const landingScreen = document.getElementById('landing-screen');
     const learnScreen = document.getElementById('learn-screen');
     const quizScreen = document.getElementById('quiz-screen');
     const reviewScreen = document.getElementById('review-screen');
+    let navButtons = document.getElementsByClassName('nav-button');
 
-    goToLanding();
-    /**
-     * Function to hide all other screens and show landing screen
-     */
-    function goToLanding() {
-        landingScreen.classList.remove('hide');
-        learnScreen.classList.add('hide');
-        quizScreen.classList.add('hide');
-        reviewScreen.classList.add('hide');
+    for (let button in navButtons) {
+        button.addEventListener('click', function(){
+            let page = this.getAttribute('data-type');
+            goToPage(page);
+        })
     }
 
-    /**
-     * Function to hide all other screens and show learn screen
-     */
-    function learnScreen() {
+    function goToPage(page) {
+        if (page === 'landing') {
+            landingScreen.classList.remove('hide');
+            learnScreen.classList.add('hide');
+            quizScreen.classList.add('hide');
+            reviewScreen.classList.add('hide');
 
-    }
+        } else if (page === 'learn') {
+            landingScreen.classList.add('hide');
+            learnScreen.classList.remove('hide');
+            quizScreen.classList.add('hide');
+            reviewScreen.classList.add('hide');
+            
+        } else if (page ==='practice') {
+            landingScreen.classList.add('hide');
+            learnScreen.classList.add('hide');
+            quizScreen.classList.remove('hide');
+            reviewScreen.classList.add('hide');
 
-    /**
-     * Function to hide all other screens and show practice screen
-     */
-    function practiceScreen() {
+        } else if (page === 'review') {
+            landingScreen.classList.add('hide');
+            learnScreen.classList.add('hide');
+            quizScreen.classList.add('hide');
+            reviewScreen.classList.remove('hide');
 
-    }
-
-    /**
-     * Function to hide all other screens and show review screen
-     */
-    function reviewScreen() {
-
+        } else {
+            alert(`Unimplemented page: ${page}`)
+            throw `Unimplemented page: ${page}. Aborted!`
+        }
     }
 })

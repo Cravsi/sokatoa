12dbac27a1b8676341c3213265001cfd1c9bb93d
Cravsi <shaun.bonner1@gmail.com>
2022-06-15 20:28:31 +0000

    [fix] navigation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,42 +11,49 @@ window.addEventListener('load', function () {
 
     for (let i = 0; i < navButtons.length; i++) {
         let page = navButtons[i].getAttribute('data-type');
+        let direction = navButtons[i].getAttribute('direction');
 
-        navButtons[i].addEventListener('click', goToPage(page))
+        navButtons[i].addEventListener('click', goToPage(page, direction))
     }
 
     /**
      * Function to select the correct page section on the HTML when the
      * corresponding button is pressed.
      */
-    function goToPage(page) {
-        if (page === 'landing') {
-            landingScreen.classList.remove('hide');
-            learnScreen.classList.add('hide');
-            quizScreen.classList.add('hide');
-            reviewScreen.classList.add('hide');
+    function goToPage(page, direction) {
+        if (!page) return;  // Guard clause
 
-        } else if (page === 'learn') {
-            landingScreen.classList.add('hide');
-            learnScreen.classList.remove('hide');
-            quizScreen.classList.add('hide');
-            reviewScreen.classList.add('hide');
-            
-        } else if (page ==='practice') {
-            landingScreen.classList.add('hide');
-            learnScreen.classList.add('hide');
-            quizScreen.classList.remove('hide');
-            reviewScreen.classList.add('hide');
-
-        } else if (page === 'review') {
-            landingScreen.classList.add('hide');
-            learnScreen.classList.add('hide');
-            quizScreen.classList.add('hide');
-            reviewScreen.classList.remove('hide');
-
-        } else {
-            alert(`Unimplemented page: ${page}`)
-            throw `Unimplemented page: ${page}. Aborted!`
+        switch (page, direction) {
+            case 'landing':
+                if (landingScreen.classList.contains('hide')) {
+                    landingScreen.classList.remove('hide'); 
+                }
+                break;
+            case 'learn':
+                if (direction === 'next') {
+                    landingScreen.classList.add('hide');
+                    learnScreen.classList.remove('hide');
+                    break;
+                }
+                quizScreen.classList.add('hide');
+                learnScreen.classList.remove('hide');
+                break;
+            case 'practice':
+                if (direction === 'next') {
+                    quizScreen.classList.add('hide');
+                    practiceScreen.classList.remove('hide');
+                    break;
+                }
+                reviewScreen.classList.add('hide');
+                practiceScreen.classList.remove('hide');
+                break;
+            case 'review':
+                if (reviewScreen.classList.contains('hide')) {
+                    reviewScreen.classList.remove('hide'); 
+                }
+                break;
+            default:
+                break;
         }
     }
 })

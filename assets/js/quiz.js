d62b5d746bf99b57ffe1c594378c3295c22511bd
Cravsi <shaun.bonner1@gmail.com>
2022-06-19 18:51:36 +0000

    [add] functions to retrieve questions and populate the quiz area

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,19 @@
-/* Trigonometry Quiz Questions */
+/** 
+ * File contains game questions and functions for retrieving this data. 
+ */
 
 const trigQuestions = [{
-    trigQuestion: 'assets/images/questions/question1.webp',
+    trigImage: 'assets/images/questions/question1.webp',
     trigAnswer: 'tan',
-    trigOptions: [sin, cos, tan, pass]
+    trigOptions: [sin, cos, tan, pass],
+    trigQuestion: 'What is the correct trigonometric funtion?'
 }]
+
+/**
+ * Returns a random question from the trigQuestions object.
+ */
+function getQuestion() {
+    let num = Math.floor(Math.random() * trigQuestions.length);
+    let questionObject = trigQuestions[num];
+    return questionObject;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,20 @@ const learnScreen = document.getElementById('learn-screen');
 const quizScreen = document.getElementById('quiz-screen');
 const reviewScreen = document.getElementById('review-screen');
 let navButtons = document.getElementsByClassName('nav-button');
+let option1 = document.getElementById('option1');
+let option2 = document.getElementById('option2');
+let option3 = document.getElementById('option3');
+let option4 = document.getElementById('option4');
+let question = document.getElementById('question');
+let quizImage = document.getElementById('quiz-image');
+let gameStart
 
 
 /**
  * Function for navigational buttons located in index.html
+ * Adds and removes the .hide class from section.
+ * display: none to hide sections
+ * display: block to show sections.
  * @param {string} button
  */
 function goToPage(button) {
@@ -48,3 +58,29 @@ function goToPage(button) {
             break;
     }
 }
+
+/**
+ * Gets a random question from the getQuestion() function
+ * in quiz.js and populates the quiz grid.
+ */
+function quizQuestion() {
+    let questionObject = getQuestion();
+    let answer = questionObject.trigAnswer;
+
+    question.innerHTML = questionObject.trigQuestion;
+    option1.innerHTML = questionObject.trigOptions[0];
+    option2.innerHTML = questionObject.trigOptions[1];
+    option3.innerHTML = questionObject.trigOptions[2];
+    option4.innerHTML = questionObject.trigOptions[3];
+    quizImage.setAttribute('src', trigImage);
+
+    return answer;
+};
+
+function toggleQuiz () {
+
+};
+
+function quizReset () {
+
+};

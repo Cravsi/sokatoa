77687b756374692df175f5119e80d8c294328ce4
Cravsi <shaun.bonner1@gmail.com>
2022-06-25 19:14:49 +0000

    [add] question 2 object, image and credits

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,4 +3,10 @@ const trigQuestions = [{
     trigAnswer: "tan",
     trigOptions: ["sin", "cos", "tan"],
     trigQuestion: "What is the correct trigonometric funtion?"
+},
+{
+    trigImage: "assets/images/questions/question2.png",
+    trigAnswer: "adjacent",
+    trigOptions: ["opposite", "adjacent", "hypotenuse"],
+    trigQuestion: "What is the name of the missing side of the triangle?"
 }]
